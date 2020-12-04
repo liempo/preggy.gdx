b872accad7afd5acf6a836acf75d7deec9156eae
Alec John <gonzalesalec@gmail.com>
2020-12-04 20:20:25 +0800

    FIx formatting of enum for better readability

--- core/src/wtf/liempo/pregnancy/breakout/SurroundingEdges.kt
+++ core/src/wtf/liempo/pregnancy/breakout/SurroundingEdges.kt
@@ -5,13 +5,24 @@ import ktx.math.vec2
 import wtf.liempo.pregnancy.Game
 import wtf.liempo.pregnancy.breakout.BreakoutUtils.translate
 
-enum class SurroundingEdges(val start: Vector2, val end: Vector2) {
-    TOP(start = vec2(0f, translate(Game.GAME_HEIGHT)), end = vec2(
-            translate(Game.GAME_WIDTH), translate(Game.GAME_HEIGHT))),
-    BOTTOM(start = vec2(0f, 0f), end = vec2(
-            translate(Game.GAME_WIDTH), 0f)),
-    LEFT(start = vec2(0f, 0f), end = vec2(0f,
-            translate(Game.GAME_HEIGHT))),
-    RIGHT(start = vec2(translate(Game.GAME_WIDTH), 0f), end = vec2(
-            translate(Game.GAME_WIDTH), translate(Game.GAME_HEIGHT)))
+enum class SurroundingEdges(
+        val start: Vector2,
+        val end: Vector2) {
+
+    CEILING(
+            start = vec2(0f, translate(Game.GAME_HEIGHT)),
+            end = vec2(translate(Game.GAME_WIDTH), translate(Game.GAME_HEIGHT)),
+    ),
+    FLOOR(
+            start = vec2(0f, translate(Game.GAME_HEIGHT * 0.10f)),
+            end = vec2(translate(Game.GAME_WIDTH), translate(Game.GAME_HEIGHT * 0.10f)),
+    ),
+    LEFT(
+            start = vec2(0f, 0f),
+            end = vec2(0f, translate(Game.GAME_HEIGHT)),
+    ),
+    RIGHT(
+            start = vec2(translate(Game.GAME_WIDTH), 0f),
+            end = vec2(translate(Game.GAME_WIDTH), translate(Game.GAME_HEIGHT)),
+    )
 }
