2cb01b3a3a787d56ec6b5cb1fa127e6383da08a6
Alec John <gonzalesalec@gmail.com>
2020-12-06 02:51:17 +0800

    Breakout can now be loaded on BreakoutUtils

--- core/src/wtf/liempo/pregnancy/breakout/BreakoutUtils.kt
+++ core/src/wtf/liempo/pregnancy/breakout/BreakoutUtils.kt
@@ -1,6 +1,26 @@
 package wtf.liempo.pregnancy.breakout
 
+import com.badlogic.gdx.graphics.Texture
+import ktx.assets.load
+import wtf.liempo.pregnancy.Game
+import wtf.liempo.pregnancy.mainmenu.MainMenuScreen
+
 object BreakoutUtils {
+
+    internal fun show(game: Game) {
+        game.run {
+            assets.let {
+                it.load<Texture>(BreakoutScreen.TEXTURE_PADDLE)
+                it.load<Texture>(BreakoutScreen.TEXTURE_BRICK)
+                it.load<Texture>(BreakoutScreen.TEXTURE_BALL)
+                it.finishLoading()
+            }
+
+            removeScreen<MainMenuScreen>()
+            setScreen<BreakoutScreen>()
+        }
+    }
+
     // Translates pixels into world units (meters)
     internal fun translate(pixels: Float) = pixels / PIXELS_PER_METER
     internal fun translate(pixels: Int) = pixels / PIXELS_PER_METER
